9fecc779d89e5b5e4858eec2e733b4d66651e24f
wagnerroserom <wrosero9059@utm.edu.ec>
2025-07-31 20:23:44 -0500

    Se inserta el DOM para interactuar con los elementos del HTML

---
+++ script.js
@@ -0,0 +1,5 @@
+//Elementos del DOM
+const btnA = document.getElementById('follow-btn-a');
+const btnB = document.getElementById('follow-btn-b');
+const timerDisplay = document.getElementById('timer');
+
